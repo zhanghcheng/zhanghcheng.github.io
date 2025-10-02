fb2aa59df9ea420bfc034ff1db92330334e7752f
CHY <chenghao@stu.njau.edu.cn>
2025-10-02 16:25:51 +0800

    Change base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/zhanghcheng.github.io/', // page
+  base: '/', // page
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
