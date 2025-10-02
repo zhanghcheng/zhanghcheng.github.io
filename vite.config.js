a8e9b8d2676c45c96dded0415124931bada93cca
ChengZhanghao <chenghao@stu.njau.edu.cn>
2025-10-02 17:19:03 +0800

    deploy with actions

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
+// 你是用户主页仓库，base 必须是 '/'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [ vue(), mode==='development' && vueDevTools() ].filter(Boolean),
-  base: '/', // page
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+export default defineConfig(({ mode }) => ({
+  base: '/',  // ⚠️ 用户主页固定用 '/'
+  plugins: [vue(), mode === 'development' && vueDevTools()].filter(Boolean),
+}))
