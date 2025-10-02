66c9fab05ced02c9f852a20a7ee8fddd9eeaac3f
CHY <chenghao@stu.njau.edu.cn>
2025-10-02 16:42:17 +0800

    Refactor plugins array to conditionally include vueDevTools

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [ vue(), mode==='development' && vueDevTools() ].filter(Boolean),
   base: '/', // page
   resolve: {
     alias: {
