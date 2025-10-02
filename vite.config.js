30c751c7692ef21d9d69e4b95d4d9497ab18ea8a
ChengZhanghao <chenghao@stu.njau.edu.cn>
2025-10-02 16:15:17 +0800

    upload

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/',
+  base: '/zhanghcheng.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
